68b586f8fa98c17076e6aa9bc2d1d21fded16299
Mr-mercedes <Ekyd777@gmail.com>
2022-12-30 21:43:28 +0300

    add task 014

---
+++ src/acmp014.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class acmp014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int x = Integer.parseInt(str[0]);
+        int y = Integer.parseInt(str[1]);
+        int countX = 0;
+        int countY = 0;
+        int result = 1;
+        for (int i = 2; i <= Math.max(x, y); i++) {
+            while (x % i == 0 && i <= x){
+                x = x / i;
+                countX++;
+            }
+            while (y % i == 0 && i <= y){
+                y = y / i;
+                countY++;
+            }
+            if (countX > 0 || countY > 0){
+                int count = Math.max(countX, countY);
+                result *= Math.pow(i, count);
+                countX = 0;
+                countY = 0;
+            }
+        }
+        System.out.println(result);
+    }
+}
