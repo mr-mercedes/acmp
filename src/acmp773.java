a06c09b69ea8f787a332b13c5b13687f6ddc625e
Mr-mercedes <Ekyd777@gmail.com>
2023-01-01 18:20:45 +0300

    add 773, 195

---
+++ src/acmp773.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class acmp773 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        int N = sc.nextInt();
+        System.out.println((K * N) * K);
+    }
+}
